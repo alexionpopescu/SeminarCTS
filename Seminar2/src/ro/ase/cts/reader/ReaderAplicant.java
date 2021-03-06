d6a6607ff4a5650366ead668f6d9edcd31286fd6
Ion-Alexandru Popescu <popescuion18@stud.ase.ro>
2021-03-06 10:43:11 +0200

    Seminar2 final commit!

---
+++ Seminar2/src/ro/ase/cts/reader/ReaderAplicant.java
@@ -0,0 +1,11 @@
+package ro.ase.cts.reader;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Aplicant;
+
+public interface ReaderAplicant {
+	public List<Aplicant> readAplicanti(String file) throws FileNotFoundException;
+		
+}
