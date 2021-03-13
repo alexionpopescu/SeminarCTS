124331c125e71ab75acfff19a8aa99ebb2f4ca19
Ion-Alexandru Popescu <popescuion18@stud.ase.ro>
2021-03-13 11:22:18 +0200

    Seminar 2+3

--- Seminar2/src/ro/ase/cts/reader/ReaderAplicant.java
+++ Seminar2/src/ro/ase/cts/reader/ReaderAplicant.java
@@ -2,6 +2,7 @@ package ro.ase.cts.reader;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 import ro.ase.cts.clase.Aplicant;
 
@@ -12,5 +13,19 @@ public abstract class ReaderAplicant {
 		super();
 		this.numeFisier = numeFisier;
 	}
-	
+	public void readAplicant(Scanner scanner,Aplicant aplicant) {
+		String nume = scanner.next();
+		String prenume = (scanner.next()).toString();
+		int varsta = Integer.valueOf(scanner.nextInt());
+		int punctaj = Integer.valueOf(scanner.nextInt());
+		int nr = Integer.valueOf(scanner.nextInt());
+		String[] vect = new String[5];
+		for (int i = 0; i < nr; i++)
+			vect[i] = scanner.next();
+		aplicant.setNume(nume);
+		aplicant.setPrenume(prenume);
+		aplicant.setVarsta(varsta);
+		aplicant.setPunctaj(punctaj);
+		aplicant.setDenumireProiect(vect,nr);
+	}
 }
