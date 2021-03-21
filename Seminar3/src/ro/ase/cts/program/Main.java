7e02a79c854bd8d117560fd73559a0e8354798ce
Ion-Alexandru Popescu <popescuion18@stud.ase.ro>
2021-03-21 16:41:24 +0200

    Seminar 4 singleton final!

--- Seminar3/src/ro/ase/cts/program/Main.java
+++ Seminar3/src/ro/ase/cts/program/Main.java
@@ -1,6 +1,7 @@
 package ro.ase.cts.program;
 
 import ro.ase.cts.singletonlazy.AsigurareMedicala;
+import ro.ase.cts.singletonlazy.Palat;
 
 public class Main {
 
@@ -11,7 +12,18 @@ public class Main {
 		asigurareMedicala1.setContributie(3.3f);
 		System.out.println(asigurareMedicala);
 		System.out.println(asigurareMedicala1);
-	
+		Palat palat1=Palat.getInstance("George", 150.5f, 100);
+		Palat palat2=Palat.getInstance("Carol", 250.5f, 200);
+		
+		
+		System.out.println(palat1.toString());
+		System.out.println(palat2.toString());
+		
+		palat1.setNumarCamere(300);
+		palat2.setInaltime(250.4f);
+		
+		System.out.println(palat1.toString());
+		System.out.println(palat2.toString());
 		
 	}
 
