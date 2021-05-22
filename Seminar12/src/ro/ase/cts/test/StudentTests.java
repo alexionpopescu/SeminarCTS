b2195ee4fd28343ec222019b32b70d061bca34ac
Ion-Alexandru Popescu <popescuion18@stud.ase.ro>
2021-05-22 11:21:00 +0300

    Seminar 12 junit basics!

--- Seminar12/src/ro/ase/cts/test/StudentTests.java
+++ Seminar12/src/ro/ase/cts/test/StudentTests.java
@@ -31,4 +31,46 @@ public class StudentTests {
 		student.adaugaNota(10);
 		assertEquals(10, student.getNota(0));
 	}
+	@Test
+	public void testDimensiuneLista() {
+		Student student=new Student();
+		student.adaugaNota(10);
+		student.adaugaNota(9);
+		assertEquals(2, student.getNote().size());
+	}
+	@Test
+	public void testCalculeazaMedie() {
+		Student student=new Student();
+		student.adaugaNota(8);
+		student.adaugaNota(6);
+		assertEquals(7f, student.calculeazaMedie(),0.1);
+	}
+	@Test
+	public void testAreRestanta() {
+		Student student=new Student();
+		student.adaugaNota(4);
+		student.adaugaNota(7);
+		assertTrue(student.areRestante());
+	}
+	@Test
+	public void testGetNotaIndexInvalid() {
+		Student student=new Student();
+		student.adaugaNota(7);
+		try {
+			int nota=student.getNota(-1);
+			fail("Nu trebuia sa ajunga aici. Metoda nu arunca exceptie");
+		}catch(IndexOutOfBoundsException e) {
+			
+		}
+		catch(Exception ex) {
+			fail("Tipul exceptiei aruncate nu este corect");
+		}
+	}
+	@Test(expected=IndexOutOfBoundsException.class) 
+	public void testGetNotaIndexInvalidJunit4() {
+		Student student=new Student();
+		student.adaugaNota(4);
+		int nota=student.getNota(-1);
+		
+	}
 }
